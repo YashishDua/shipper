676c2064f6ff86f12cbfd66f0865cf763e8fc897
Yashish Dua <yashishdua@Yashishs-MacBook-Pro.local>
2019-09-01 17:38:01 +0530

    ref: refactored basic example

--- config.go
+++ config.go
@@ -3,7 +3,7 @@ package shipper
 type Config struct {
 	Source      string // Source file path
 	Destination string // Destination file path
-	BatchSize   int    // Total characters to be processed in 1 async batch
+	BatchSize   int    // Total characters to be processed in 1 concurrent batch
 	TCP         TCP
 }
 
--- example/basic/main.go
+++ example/basic/main.go
@@ -10,10 +10,6 @@ func main() {
 		Source:      "test.log",
 		Destination: "test2.log",
 		BatchSize:   5, // Total characters to be processed in 1 concurrent batch
-		TCP: shipper.TCP{
-			Host: "localhost",
-			Port: 8000,
-		},
 	})
 
 	if err := shipper.ShipAndDock(); err != nil {
